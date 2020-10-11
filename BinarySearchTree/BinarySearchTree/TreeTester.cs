950d4b34085d463b885646579e5b429d423b7b22
Tshmofen <timohatimafeikq@gmail.com>
2020-10-12 00:45:33 +0300

    Added binary tree tester

--- BinarySearchTree/BinarySearchTree/TreeTester.cs
+++ BinarySearchTree/BinarySearchTree/TreeTester.cs
@@ -7,18 +7,52 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             BinaryTree tree = new BinaryTree();
-            int[] nodes = { 1, 2, 9, 6, 7, 4, 10 };
+            int[] nodes = { 1, 11, 2, 9, 6, 7, 4, 10 };
             foreach (int num in nodes) tree.AddNode(num);
 
+            // Tree building
             TreePrinter.PrintChildNodes(tree.RootNode);
             Console.ReadKey();
-            Console.Clear();
+            Console.Write('\b');
 
+            // Traversals
+            Console.Write("Traversals:\n1)Preorder { ");
+            tree.PreorderTraversal();
+            Console.Write("}\n2)Inorder { ");
+            tree.InorderTraversal();
+            Console.Write("}\n3)Postorder { ");
+            tree.PostorderTraversal();
+            Console.WriteLine("}\n");
+
+            Console.ReadKey();
+            Console.Write("\b");
+
+            // K-min element
+            Console.Write("Finding of k-min element:\nEnter k = ");
+            int k = 0;
+            for(int i = 0; i < 1; i++)
+            {
+                try
+                {
+                    k = int.Parse(Console.ReadLine());
+                    if (k < 1 || k > nodes.Length) throw new Exception();
+                }
+                catch(Exception)
+                {
+                    Console.Write("k = ");
+                    i--;
+                }
+            }
+            Console.WriteLine($"{k}-min element is {tree.FindMinimalNode(k)}\n");
+            Console.ReadKey();
+            Console.Write("\b");
+
+            // Tree balancing
+            Console.WriteLine("Original tree balanced by rotations:");
             tree.BalanceTree();
 
             TreePrinter.PrintChildNodes(tree.RootNode);
             Console.ReadKey();
-            Console.Clear();
 
         }
     }
